f296eb8d7063a44f0abe7518fa2520b523c8a07b
mtsgi <oq@live.jp>
2021-12-11 23:25:18 +0900

    Init vite + svelte app

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Generator from './lib/Generator.svelte';
+</script>
+
+<main>
+  <h1>UUID Generator</h1>
+
+  <Generator />
+
+  <footer>
+    mtsgi/uuidgen
+  </footer>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    line-height: 1.5;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    color: var(--text);
+
+    --text: #505050;
+    --theme: #4987c0;
+    --sub: #bcdeff;
+  }
+
+  h1 {
+    color: var(--theme);
+    font-size: 2.5rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 0 0 2rem 0;
+  }
+
+  footer {
+    color: var(--text);
+    margin: 0 0 1rem 0;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 3rem;
+    }
+  }
+</style>
---
+++ src/lib/Generator.svelte
@@ -0,0 +1,45 @@
+<script>
+  let uuids = []
+  const generate = () => {
+    // @ts-ignore
+    uuids = [...uuids, self.crypto.randomUUID()];
+  }
+</script>
+
+<button on:click={generate}>
+  Generate { uuids.length }
+</button>
+
+<ul>
+  { #each uuids as uuid }
+    <li>{ uuid }</li>
+  {/each}
+</ul>
+
+<style>
+  button {
+    font-family: inherit;
+    font-size: 1rem;
+    padding: 1em;
+    color: var(--theme);
+    background-color: var(--sub);
+    border-radius: 8px;
+    border: 2px solid transparent;
+    outline: none;
+    cursor: pointer;
+    margin: 0 0 1rem 0;
+  }
+
+  button:active {
+    border: 2px solid var(--theme);
+  }
+
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+
+  li {
+    margin: 0 0 0.5rem 0;
+  }
+</style>
